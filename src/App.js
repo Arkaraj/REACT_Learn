3f8cba1ab175620a6b970b71eda71fe4d66afd8a
Arkaraj <arkaraj2017@gmail.com>
2021-02-01 20:47:33 +0530

    States and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
+import Nav from './components/Nav'
+import Posts from './components/Posts'
 
 function App() {
+  // Write any JavaScript here
   // States
-  let state = {
-    name: 'Arkaraj',
-    age: 18
-  }
+  // let state = {
+  //   name: 'Arkaraj',
+  //   age: 18
+  // }
   // For class this.state.name
+
+  const name = 'Code1234'
+  const code = '$ npm install'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Heading</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br />
+    <div className="App" className="App-header">
+      <Nav />
+      {/* <img src={logo} className="App-logo" alt="logo" />
+      <h1>H</h1>
+      <p>
+        Edit <code>src/App.js</code> and save to reload. <br />
           Random number: {Math.random() * 10} <br />
           My name is {state.name}, I am {state.age}
-        </p>
-
-      </header>
+      </p> */}
+      <Posts article={name} code={code} />
     </div>
   );
 }
