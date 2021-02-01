f5c09bae51c9c96c2f5f003da582c469e7b34c80
Arkaraj <arkaraj2017@gmail.com>
2021-02-01 23:51:34 +0530

    useStates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import './App.css';
 import Nav from './components/Nav'
 import Posts from './components/Posts'
@@ -14,9 +15,24 @@ function App() {
   const name = 'Code1234'
   const code = '$ npm install'
 
+  // States and be passed down as props as well
+  let [counter, setCounter] = useState(0)
+  let [toggle, setToggle] = useState(false)
+
+
+  const increment = () => {
+    // counter += 1;
+    setCounter(prev => prev + 1) //setCounter(counter + 1) also works
+    // console.log(counter)
+  }
+
+  const visible = () => {
+    setToggle(prev => !prev)
+  }
+
   return (
     <div className="App" className="App-header">
-      <Nav />
+      {/* <Nav /> */}
       {/* <img src={logo} className="App-logo" alt="logo" />
       <h1>H</h1>
       <p>
@@ -24,7 +40,11 @@ function App() {
           Random number: {Math.random() * 10} <br />
           My name is {state.name}, I am {state.age}
       </p> */}
-      <Posts article={name} code={code} />
+      {/* <Posts article={name} code={code} /> */}
+      <h1 className={toggle ? "hide" : ""}>React</h1> <br />
+      <h1>Couter: {counter}</h1> <br />
+      <button onClick={increment}>Increment</button>
+      <button onClick={visible}>Toggle</button>
     </div>
   );
 }
